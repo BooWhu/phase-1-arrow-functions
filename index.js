14e1cbeaf739fdf1f9e730de5d8fdf9e11f35e58
Eli <emmuzio0510@icloud.com>
2023-11-06 19:05:14 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,22 @@
+// Define the divide function
+function divide() {
+    return 2000 / 100;
+  }
+  
+  // Define the square function as an arrow function
+  const square = (num) => {
+    return num * num;
+  }
+  
+  // Define the add function as an arrow function
+  const add = (a, b) => {
+    return a + b;
+  }
+  
+  // Export the functions if needed
+  module.exports = {
+    divide,
+    square,
+    add,
+  };
+  
